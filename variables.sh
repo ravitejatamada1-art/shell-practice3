18f6eeb4574b533f278e95e25b288a2f6c9cde8c
Raviteja.T <ravitejatamada1@gmail.com.com>
2026-05-23 16:22:25 +0530

    runner created

--- variables.sh
+++ variables.sh
@@ -3,7 +3,7 @@
 person1 = ravi
 person2 = raju
 
-echo "$person : ala vunnav ra"
+echo "$person1 : ala vunnav ra"
 echo "$person2 : super ga vunna ra"
 echo "$person1 : tinnava ra"
 echo "$person2 : nuv tinnava"
