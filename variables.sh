8bb86ab06cef7d3e1e670b291d0ec426160226e7
Raviteja.T <ravitejatamada1@gmail.com.com>
2026-05-23 16:53:48 +0530

    01.variables.sh created

--- variables.sh
+++ variables.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-person1=$1
-person2=$2
-
-echo "$person1 : ala vunnav ra"
-echo "$person2 : super ga vunna ra"
-echo "$person1 : tinnava ra"
-echo "$person2 : nuv tinnava"
-echo "$person1 :ha tinna ra"
+echo "enter your pin number :"
+read Spin
+echo "your pin is : $pin"
