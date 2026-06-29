375bb118bf980fb1982ca481a4e803b4e61246d7
Raviteja.T <ravitejatamada1@gmail.com.com>
2026-06-29 16:40:09 +0530

    conditions created

--- 19.conditions.sh
+++ 19.conditions.sh
@@ -1,9 +1,9 @@
 NUMBER=$1
 if [NUMBER -lt 10]
 {
-   echo "number is less than 10" 
+   echo "$NUMBER is less than 10" 
 }
 else
 {
-    echo "number is greater than or equal to 10"
+    echo "$NUMBER is greater than or equal to 10"
 }
