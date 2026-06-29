b6fb9b3ea59ec565b3d202e3689c55d3f221e21d
Raviteja.T <ravitejatamada1@gmail.com.com>
2026-06-29 16:06:59 +0530

    special variables.sh created

--- special variables.sh
+++
@@ -1,9 +0,0 @@
-echo"current home directory name : $HOME"
-echo"current user : $USER"
-echo"current PID is : $$"
-sleep 10&
-echo"background PID IS : $!"
-echo"total number of arg is : $#"
-echo"name of file is : $0"
-echo"total args : $@"
-echo"current working directory : $PWD"
