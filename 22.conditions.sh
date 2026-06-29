2447f9f5039f20c17bd6956f3672269283e676f5
Raviteja.T <ravitejatamada1@gmail.com.com>
2026-06-29 21:21:41 +0530

    22.conditions.sh created

--- 22.conditions.sh
+++ 22.conditions.sh
@@ -6,6 +6,14 @@ exit 1
 else
 echo "install nginx"
 fi
+if [ $USERID -eq 0]
+then
+dnf install nginx -y
+echo " nginx installed successfully"
+else
+echo "failed to install nginx"
+exit 1
+fi
 
 #echo "failed to install python"
 #else
