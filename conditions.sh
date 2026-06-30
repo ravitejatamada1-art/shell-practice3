6079dd8e78d8e330e763e1269e5ceb69e7d15f11
Raviteja.T <ravitejatamada1@gmail.com.com>
2026-06-30 20:27:57 +0530

    conditions created

--- conditions.sh
+++ conditions.sh
@@ -3,7 +3,6 @@ then
 echo "Installing mysql in mobx"
 else
 echo "installing failed of mobx"
-exit 1
 fi
 dnf install mysql -y
 exit 1
@@ -12,4 +11,5 @@ then
 echo "installed mysql successfully"
 else
 echo "failed of installing mysql"
+exit 1
 fi
