60b4a7d7ed13668f4e5178938cc2d57bc8b51f3f
Raviteja.T <ravitejatamada1@gmail.com.com>
2026-06-30 20:33:40 +0530

    conditions created

--- conditions.sh
+++ conditions.sh
@@ -1,11 +1,11 @@
-if  [ $USERID -eq 0 ]
+if [ $USERID -eq 0 ]
 then
 echo "Installing mysql in mobx"
 else
 echo "installing failed of mobx"
 fi
 dnf install mysql -y
-if  [ $? -eq 0 ]
+if [ $? -eq 0 ]
 then
 echo "installed mysql successfully"
 else
