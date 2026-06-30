a32f474ec550aead692e9d008e1f1fc8f9ea0272
Raviteja.T <ravitejatamada1@gmail.com.com>
2026-06-30 21:21:04 +0530

    conditions created

--- 23.conditions.sh
+++ 23.conditions.sh
@@ -3,14 +3,12 @@ if [ $? -eq 0 ]
 then
  echo "install nginx successfully"
 else
-exit 1
 echo "install nginx failed"
 fi
-
 if [ $? -ne 0 ]
 then 
-echo "bad day "
+echo "good day "
 else
-echo "good day"
+echo "bad day"
 fi
 
