80364bb2e61f27c6144fef1db4abbd2c0cdb4429
Raviteja.T <ravitejatamada1@gmail.com.com>
2026-07-05 09:50:50 +0530

    for

--- for.sh
+++ for.sh
@@ -1,4 +1,15 @@
+
+validation()
+{
+    if [ $? -eq 0 ]
+    then 
+    echo "installed $Ravi is successful"
+    else
+    echo "install of $ Ravi is failed"
+    fi
+}
 for Ravi in {nginx,python3,mysql}
 do
 dnf install $Ravi -y
+validation ()
 done
