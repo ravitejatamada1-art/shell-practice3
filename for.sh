94e3d2c4c60a065be68d491826b581841aa4561e
Raviteja.T <ravitejatamada1@gmail.com.com>
2026-07-05 10:20:17 +0530

    for

--- for.sh
+++ for.sh
@@ -8,12 +8,13 @@ validation()
     echo -e"install of \e[31m$2\e[0m is failed"
     fi
 }
+
+for Ravi in {nginx,python3,mysql}
+do
 sudo mkdir -p /var/log/shell-logs
 folder_name=shell_logs
 script_name=$logs.log
 file_name=$shell_logs/$script_name
-for Ravi in {nginx,python3,mysql}
-do
 dnf install $Ravi -y | tee -a $file_name
 validation $? $Ravi
 done
