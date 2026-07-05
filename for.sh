e5c2a2cee86fc4c94ab87b731bf83acd40beafc3
Raviteja.T <ravitejatamada1@gmail.com.com>
2026-07-05 14:05:00 +0530

    for created

--- for.sh
+++ for.sh
@@ -3,18 +3,18 @@ validation()
 {
     if [ $1 -eq 0 ]
     then 
-    echo -e "installed \e[35m$2\e[0m is successful"
+    echo -e "installed \e[35m$2\e[0m is successful" | tee -a $file_name
     else
-    echo -e"install of \e[31m$2\e[0m is failed"
+    echo -e"install of \e[31m$2\e[0m is failed" | tee -a $file_name
     fi
 }
 
 for Ravi in {nginx,python3,mysql}
 do
-sudo mkdir -p /var/log/shell-logs
-folder_name=shell_logs
-script_name=$logs.log
-file_name=$shell_logs/$script_name
+sudo mkdir -p shell-logs
+folder_name="/var/log/shell-logs"
+script_name=$logs.log 
+file_name="$folder_name/$script_name.log"
 dnf install $Ravi -y | tee -a $file_name
 validation $? $Ravi
 done
