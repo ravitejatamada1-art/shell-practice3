e06acf1963bd6611f1e1d6df55666546e20fef9d
Raviteja.T <ravitejatamada1@gmail.com.com>
2026-07-05 09:56:17 +0530

    for

--- for.sh
+++ for.sh
@@ -3,9 +3,9 @@ validation()
 {
     if [ $1 -eq 0 ]
     then 
-    echo "installed $Ravi is successful"
+    echo "installed \e[35m $Ravi \e[0m is successful"
     else
-    echo "install of $ Ravi is failed"
+    echo "install of $Ravi is failed"
     fi
 }
 for Ravi in {nginx,python3,mysql}
