b4672c41839bd7dd68796422eca9e83fe217415c
Raviteja.T <ravitejatamada1@gmail.com.com>
2026-07-06 15:51:07 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -6,7 +6,7 @@ ZONE_ID=Z08964191DFKFWEA2YRY0
 DOMAIN_NAME="roboshop.bond"
 for instance in ${INSTANCES[@]}
 do 
-  INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-026381a5a2005e8e7 --tag-specifications "ResourceType=instance,tags=[{Key=Name,Value=$instance}]" --query "Instances[0].InstanceID" --output text)
+  INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-026381a5a2005e8e7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query "Instances[0].InstanceID" --output text)
   if [ $instance != "FRONTEND" ]
   then
       IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
