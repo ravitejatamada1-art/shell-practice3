2d0a065851c9fd77a6263c2bda2e1f4bdd83032e
Raviteja.T <ravitejatamada1@gmail.com.com>
2026-07-06 15:25:18 +0530

    roboshop

---
+++ roboshop.sh
@@ -0,0 +1,17 @@
+31/bin/bash
+AMI_ID="ami-0220d79f3f480ecf5"
+SG_ID="sg-026381a5a2005e8e7"
+INSTANCES=("MONGODB" "REDIS" "MYSQL" "RABBITNQ" "CATALOGUE" "USER" "CART" "SHIPPING" "PAYMENT" "DISPATCH" "FRONTEND")
+ZONE_ID=Z08964191DFKFWEA2YRY0
+DOMAIN_NAME="roboshop.bond"
+for instance in ${INSTANCES[@]}
+do 
+INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t2.micro --security-type-ids sg-026381a5a2005e8e7 --tag-specifications "ResourceType=instance,tags=[{Key=Name,value=$instance}] --query "Instances[0].INSTANCEID" --output text)
+  if [ $instance != frontend" ]
+  then
+      IP=$(aws ec2 describe-instance --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress --output.text)
+else
+     IP=$(aws ec2 describe-instancess --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress --output text)
+     fi
+
+done
