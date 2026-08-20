3aa67961549c73cec76f052d04a1e115553e9e89
Raviteja.T <ravitejatamada1@gmail.com.com>
2026-08-20 13:48:47 +0530

    logs deleted

--- roboshop.sh
+++ roboshop.sh
@@ -6,12 +6,12 @@ ZONE_ID=Z08964191DFKFWEA2YRY0
 DOMAIN_NAME="roboshop.bond"
 for instance in ${INSTANCES[@]}
 do 
-  INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t2.micro --security-group-ids sg-026381a5a2005e8e7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query "Instances[0].InstanceID" --output text)
+  INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-026381a5a2005e8e7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query "Instances[0].InstanceId" --output text)
   if [ $instance != "frontend" ]
   then
       IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
   else
       IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
      fi
-     echo "$instance Ip Address:$IP"
+     echo "$instance IP Address:$IP"
 done
