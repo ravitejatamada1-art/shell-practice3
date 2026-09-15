acd446b53f390181fa4b0f8ef478e29247e069e7
Raviteja.T <ravitejatamada1@gmail.com.com>
2026-09-15 15:08:40 +0530

    OLDLOGS DELETED

---
+++ oldlogs.sh
@@ -0,0 +1,6 @@
+SOURCE_DIR=/home/ec2-user/shell-practice3/Ravi-logs1
+DELETE_FILES= (find SOURCE_DIR -name "*.log" -mtime +30)
+while read -r filepath
+do
+rm -rf $filepath
+done <<< "$(DELETE_FILES)"
