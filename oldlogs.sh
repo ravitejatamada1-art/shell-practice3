33a3b367fb094bbdd489e6b3170712a2eb8503ef
Raviteja.T <ravitejatamada1@gmail.com.com>
2026-09-15 15:34:43 +0530

    OLDLOGS DELETED

--- oldlogs.sh
+++ oldlogs.sh
@@ -2,7 +2,7 @@ LOG_FILE=$LOG_FOLDER/ravi1.log
 LOG_FOLDER=/var/log/Ravi-logs2
 mkdir -p $LOG-FOLDER
 
-if [ $USERID -eq 0 ]
+if [ $USERID - eq 0 ]
 then 
 echo "you are Running with Root User" |tee -a $LOG_FILE
 else
@@ -11,7 +11,3 @@ fi
 echo "script started executing at $(date)" |tee -a $LOG_FILE
 SOURCE_DIR=/home/ec2-user/shell-practice3/Ravi-logs1
 DELETE_FILES= find $SOURCE_DIR -name "*.log" -mtime +30
-while IFS= read -r filepath
-do
-rm -rf $filepath
-done <<< "$DELETE_FILES"
